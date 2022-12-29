8ff35d0927c3b93a74e1a32b75544f7f8f861bc7
baibhavrajput <baibhavrajputt@gmail.com>
2022-12-29 16:54:44 +0530

    initial commit

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,8 +1,39 @@
+import { useState } from 'react';
+import birdImage from './svg/bird.svg';
+import catImage from './svg/cat.svg';
+import cowImage from './svg/cow.svg';
+import dogImage from './svg/dog.svg';
+import gatorImage from './svg/gator.svg';
+import heartImage from './svg/heart.svg';
+import horseImage from './svg/horse.svg';
+
+const svgMap = {
+    bird: birdImage,
+    cat: catImage,
+    cow: cowImage,
+    dog: dogImage,
+    gator: gatorImage,
+    // heart: heartImage,
+    horse: horseImage
+}
+
 function AnimalShow(props){
     const type = props.type;
+    const [clicks , setClicks] = useState(0);
+
+    const onClickHandler = () =>{
+        setClicks(clicks+1);
+    };
 
     return (
-        <div>{type}</div>
+        <div onClick={onClickHandler}>
+            <img src={svgMap[type]} alt="animal" />
+            <img 
+            src={heartImage} 
+            alt="heart"
+            style={{width: 10 + 10 * clicks + 'px'}} 
+            />
+        </div>
     )
 }
 
